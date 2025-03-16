e75ec078da344e8043f6a53ee987bafe62fd6db1
Priscila <pnmelo2808@gmail.com>
2025-03-16 19:54:33 -0300

    feat: implement service layer with account number validation

---
+++ src/main/java/me/dio/decola_tech_2025/domain/service/UserService.java
@@ -0,0 +1,8 @@
+package me.dio.decola_tech_2025.domain.service;
+
+import me.dio.decola_tech_2025.domain.model.User;
+
+public interface UserService {
+    User findById(Long id);
+    User create(User userToCreate);
+} 
---
+++ src/main/java/me/dio/decola_tech_2025/domain/service/impl/UserServiceImpl.java
@@ -0,0 +1,31 @@
+package me.dio.decola_tech_2025.domain.service.impl;
+
+import me.dio.decola_tech_2025.domain.model.User;
+import me.dio.decola_tech_2025.domain.repository.UserRepository;
+import me.dio.decola_tech_2025.domain.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+} 
