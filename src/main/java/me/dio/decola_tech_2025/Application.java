f9e8f988146b294443a6afbf8e230cbb3e0c17d7
Priscila <pnmelo2808@gmail.com>
2025-03-16 21:11:10 -0300

    feat: adiciona OpenAPIDefinition com configuração de servidor padrão

--- src/main/java/me/dio/decola_tech_2025/Application.java
+++ src/main/java/me/dio/decola_tech_2025/Application.java
@@ -2,7 +2,12 @@ package me.dio.decola_tech_2025;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 
+@OpenAPIDefinition(
+	servers = {@Server(url = "/", description = "Default Server URL")}
+)
 @SpringBootApplication
 public class Application {
 
