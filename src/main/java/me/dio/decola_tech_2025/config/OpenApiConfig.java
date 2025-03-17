eb434ac4d0336be80562b36ed0fd679573a224cf
Priscila <pnmelo2808@gmail.com>
2025-03-16 21:00:05 -0300

    fix: corrige configuração do OpenAPI e versão do Spring Boot

---
+++ src/main/java/me/dio/decola_tech_2025/config/OpenApiConfig.java
@@ -0,0 +1,23 @@
+package me.dio.decola_tech_2025.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RESTful API do Decola Tech 2025")
+                        .description("API REST para gerenciamento de usuários do Decola Tech 2025")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+    }
+} 
